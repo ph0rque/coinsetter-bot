d24efe716cc29c431869d227840a239eda611554
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2015-01-30 10:33:37 -0500

    more tweaks

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ end
 get '/login_to_coinsetter' do
   url, parameters = SecretConfig.coinsetter_url, SecretConfig.coinsetter_login_params(@ip)
   RestClient.post(url, parameters) do |response, request, result, &block|
-    @msg = { status: 'info', content: (response + result)}
+    @msg = { status: 'info', content: response}
   end
   
   erb :login_to_coinsetter, :layout => false
