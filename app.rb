e5043b4331f2540f72d2a788b5ac3a99b1db24b6
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2015-01-30 10:29:11 -0500

    more tweaks

--- app.rb
+++ app.rb
@@ -20,12 +20,10 @@ end
 get '/login_to_coinsetter' do
   url, parameters = SecretConfig.coinsetter_url, SecretConfig.coinsetter_login_params(@ip)
   RestClient.post(url, parameters) do |response, request, result, &block|
-puts response
-puts result
-    @msg = { status: 'info', content: response }
+    @msg = { status: 'info', content: (response + result)}
   end
   
-  erb :'login_to_coinsetter.js'
+  erb :login_to_coinsetter, :layout => false
 end
 
 def get_static_ip
